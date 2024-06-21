83ae84f3af5d958f5346dc025a2e14bda454896b
dougelam <dougelam0@gmail.com>
2024-06-21 16:41:32 +0700

    test

---
+++ utils/seo.ts
@@ -0,0 +1,41 @@
+import { Metadata } from "next";
+
+export type TSEO = {
+  title: string;
+  keywords?: string[];
+  description: string;
+  thumbnail: string;
+};
+
+export const defaultSEO: TSEO = {
+  title: "Oracle Meme – Revolutionize Meme Culture with Blockchain Technology",
+  description:
+    "Join Oracle Meme in redefining meme culture with our blockchain-based platform. Participate in our presale to get early access to OMEME tokens and engage with a vibrant community of creators and investors.",
+  thumbnail: "/meta-image.png",
+};
+
+export function generateMetadata({
+  title,
+  description,
+  thumbnail,
+  keywords,
+}: Partial<TSEO> = {}): Metadata {
+  return {
+    title: title ?? defaultSEO.title,
+    description: description ?? defaultSEO.description,
+    metadataBase: new URL("https://oraclememe.com/"),
+    keywords: keywords ?? defaultSEO.keywords,
+    twitter: {
+      title: title ?? defaultSEO.title,
+      description: description ?? defaultSEO.description,
+      images: [thumbnail ?? defaultSEO.thumbnail],
+      site: "@oraclememecoin",
+    },
+    openGraph: {
+      title: title ?? defaultSEO.title,
+      description: description ?? defaultSEO.description,
+      images: [thumbnail ?? defaultSEO.thumbnail],
+      siteName: "@oraclememecoin",
+    },
+  };
+}
